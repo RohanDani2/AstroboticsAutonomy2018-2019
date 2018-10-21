14dc029b1e918bd4d56d51d0fc3940afc78dcafe
HMDekker <helend@vt.edu>
2018-10-21 17:46:23 -0400

    you spin me right round in the correct direction

--- matlab/10.16.18/positionThenDetect.m
+++ matlab/10.16.18/positionThenDetect.m
@@ -1,59 +1,85 @@
 function [cone, robot, detected] = positionThenDetect(allObstacles, command)
-
-    persistent xpos ypos  
+    persistent xpos ypos position base_cone base_robot
+    
+    
     if isempty(xpos)
         xpos = 2000;
-        ypos = 2000; 
+        ypos = 2000;
+        position = 1;
+        % Base vision cone 
+        line1 = [0,150,0; 300,450,0;];
+        line2 = [0,150,0; -300,450,0;];
+        line3 = [-300,450,0; 300,450,0;];
+        base_cone = vertcat(line1, line2, line3);
+
+        % Base robot     
+        robot_line1 = [0,150,0; 100, 50,0;];
+        robot_line2 = [100, 50, 0; 100, -150, 0;];
+        robot_line3 = [100, -150, 0; -100, -150, 0;];
+        robot_line4 = [-100,-150,0; -100, 50, 0;];
+        robot_line5 = [-100, 50, 0; 0, 150, 0;];
+        base_robot = vertcat(robot_line1, robot_line2, robot_line3, robot_line4, robot_line5);
+
     end
-    
-    % Base vision cone 
-    line1 = [0,150,0; 300,450,0;];
-    line2 = [0,150,0; -300,450,0;];
-    line3 = [-300,450,0; 300,450,0;];
-    base_cone = vertcat(line1, line2, line3);
-    
-    % Base robot     
-    robot_line1 = [0,150,0; 100, 50,0;];
-    robot_line2 = [100, 50, 0; 100, -150, 0;];
-    robot_line3 = [100, -150, 0; -100, -150, 0;];
-    robot_line4 = [-100,-150,0; -100, 50, 0;];
-    robot_line5 = [-100, 50, 0; 0, 150, 0;];
-    base_robot = vertcat(robot_line1, robot_line2, robot_line3, robot_line4, robot_line5);
-    
-    % Translate base cone and base robot based on x and y position
-    if command == 1 % forward
-        ypos = ypos + 500;
-    elseif command == 2 % backward
-        ypos = ypos - 500;
-    elseif command == 3 % CW rotate
-        xpos = xpos - 500;
-    elseif command == 4 % CC rotate 
-        xpos = xpos + 500;
+    if command ~= 0 
+        % Translate base cone and base robot based on x and y positionif command == 1
+        % rotate it
+        if command == 1 % forwards
+            if position == 1 % "up"
+                ypos = ypos + 50;
+            elseif position == 2 % "right"
+                xpos = xpos + 50;
+            elseif position == 3 % "down"
+                ypos = ypos -50;
+            elseif position == 4 % "left"
+                xpos = xpos - 50;
+            end
+        elseif command == 2 % backwards
+            if position == 1 % "up"
+                ypos = ypos - 50;
+            elseif position == 2 % "right"
+                xpos = xpos - 50;
+            elseif position == 3 % "down"
+                ypos = ypos + 50; 
+            elseif position == 4 % "left"
+                xpos = xpos + 50;
+            end
+        elseif command == 3 % cw
+            [base_cone2, base_robot2] = rowtayshun (base_cone, base_robot, 3, position);
+            position = position+1;
+            if position == 0
+                position = 4;
+            end
+            if position == 5
+                position = 1;
+            end
+            base_cone = base_cone2;
+            base_robot = base_robot2;
+        elseif command == 4 % cc
+            [base_cone2, base_robot2] = rowtayshun (base_cone, base_robot, 4, position);
+            position = position-1; 
+            if position == 0
+                position = 4;
+            end
+            if position == 5
+                position = 1;
+            end
+            base_cone = base_cone2;
+            base_robot = base_robot2;
+        end
     end
-    % for rotation: come up with a set roation dependent on var and
-    %  then make other rotation negative
-    % for facing opposite direction: turn 180
-    % could do eight cases 
-    [base_cone2, base_robot2] = rowtayshun(base_cone,base_robot,command);
-    cone = base_cone2;
-    cone(:,1) = base_cone2(:,1) + xpos;
-    cone(:,2) = base_cone2(:,2) + ypos;
-    cone(:,3) = base_cone2(:,3);
-   
-   
-    robot = base_robot2;
-    
-    robot(:,1) = base_robot2(:,1) + xpos;
-    robot(:,2) = base_robot2(:,2) + ypos;
-    robot(:,3) = base_robot2(:,3);
-    
-    % should concat robot beforehand, like with cone
-   % robot = fliplr(rot90(robot,3));
-    
-    %put rotation here--make function in different file
+
+    cone = base_cone;
+    cone(:,1) = base_cone(:,1) + xpos;
+    cone(:,2) = base_cone(:,2) + ypos;
+    cone(:,3) = base_cone(:,3);
+
+    robot = base_robot;
+    robot(:,1) = base_robot(:,1) + xpos;
+    robot(:,2) = base_robot(:,2) + ypos;
+    robot(:,3) = base_robot(:,3);
 
     % Check if obstacle is in vision cone 
     detected = detectObject(allObstacles, cone); 
-    
     
 end
--- matlab/10.16.18/rowtayshun.m
+++ matlab/10.16.18/rowtayshun.m
@@ -1,46 +1,91 @@
-function [cone, robot] = rowtayshun(cone,robot,command,last_command)
+function [cone, robot] = rowtayshun (cone, robot, command, position)
 
 tempBot = robot;
-tempCone = cone; 
-%robot
-if (command == 1)  %up  
-    robot(:,2) = tempBot(:,2);
+tempCone = cone;
+if command == 3 % cw
+    if position == 1 % "up"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX;
+    elseif position == 2 % "right"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX*-1;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX*-1;
+    elseif position == 3 % "down"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX;
+    elseif position == 4 % "left"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX*-1;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX*-1;
+    end
+elseif command == 4 % ccw
+    
+    if position == 1 % "up"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY*-1;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY*-1;
+        cone(:,2) = tempX;
+    elseif position == 2 % "right"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX;
+    elseif position == 3 % "down"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY*-1;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY*-1;
+        cone(:,2) = tempX;
+    elseif position == 4 % "left"
+        tempY = tempBot(:,2);
+        tempX = tempBot(:,1);
+        robot(:,1) = tempY;
+        robot(:,2) = tempX;
+        
+        tempY = tempCone(:,2);
+        tempX = tempCone(:,1);
+        cone(:,1) = tempY;
+        cone(:,2) = tempX;
+    end
 end
-if (command == 2)  % down
-    robot(:,2) = tempBot(:,2)*-1; 
-end
-if (command == 3)  %left
-    tempY = tempBot(:,2);
-    tempX = tempBot(:,1);
-    robot(:,1) = tempY*-1;
-    robot(:,2) = tempX;
-end
-if command == 4 %right
-    tempY = tempBot(:,2);
-    tempX = tempBot(:,1);
-    robot(:,1) = tempY;
-    robot(:,2) = tempX; 
-end
-
-%cone
-if (command == 1)  %up  
-    cone(:,2) = tempCone(:,2);
-end
-if (command == 2)  % down
-    cone(:,2) = tempCone(:,2)*-1; 
-end
-if (command == 3)  %left
-    tempY = tempCone(:,2);
-    tempX = tempCone(:,1);
-    cone(:,1) = tempY*-1;
-    cone(:,2) = tempX;
-end
-if command == 4 %right
-    tempY = tempCone(:,2);
-    tempX = tempCone(:,1);
-    cone(:,1) = tempY;
-    cone(:,2) = tempX; 
-end
-end
-
-
+end 
