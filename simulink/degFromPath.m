5da6747d701917ff874e811a251c16b7ed667864
alpetrovsky <acp2124@vt.edu>
2018-10-24 17:02:42 -0400

    added second chart for autonomy

--- simulink/degFromPath.m
+++ simulink/degFromPath.m
@@ -1,7 +1,7 @@
 function angle = degFromPath(endPt, xpos, ypos, theta)
     x = endPt(1) - xpos;
     y = endPt(2) - ypos;
-    angle = atan(y/x) - theta;
+    angle = -(atan(y/x) - theta);
     if angle >= pi
         angle = angle - 2*pi;
     elseif angle <= -pi
