de2bdae23c310b2e8c18b50354f39399cd49726f
alpetrovsky <acp2124@vt.edu>
2018-10-29 02:18:22 -0400

    working level 1 autonomy

--- simulink/degFromPath.m
+++ simulink/degFromPath.m
@@ -2,9 +2,8 @@ function angle = degFromPath(endPt, xpos, ypos, theta)
     x = endPt(1) - xpos;
     y = endPt(2) - ypos;
     angle = -(atan(y/x) - theta);
+    angle = mod(angle, 2*pi);
     if angle >= pi
         angle = angle - 2*pi;
-    elseif angle <= -pi
-        angle = angle + 2*pi;
     end
 end
