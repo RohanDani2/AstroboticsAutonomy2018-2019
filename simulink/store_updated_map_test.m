477015d2b6101bf65559d74750afb4881849e610
alpetrovsky <acp2124@vt.edu>
2018-12-03 11:55:02 -0500

    changed condition to enter map update block, now makes list of known obstacles

--- simulink/store_updated_map_test.m
+++ simulink/store_updated_map_test.m
@@ -1,38 +1,39 @@
 global occupancyMap
-xmax = 10; % Physical wall just outside these distances 
-ymax = 10; 
+xmax = 5; % Physical wall just outside these distances 
+ymax = 5; 
 obstList = zeros(ymax*xmax,2);
 
 fov = zeros([10 2]);
 fov_record = zeros([xmax*ymax 2]);
 n = 20;
 if isempty(occupancyMap)
-    occupancyMap = 0;
+    occupancyMap = logical(zeros(ymax, xmax));
 end
 detected = randi([0 1], 1, n);
 allObstX = randi([1 xmax], n, 1);
 allObstY = randi([1 ymax], n, 1);
 
-% fov(:,1) = allObstX(detected==1); 
-% fov(:,2) = allObstY(detected==1);
-% fov_record(:,1) = fov_record(:,1) | fov(:,1);
-% fov_record(:,2) = fov_record(:,2) | fov(:,2);
+if any(detected)
+    a = allObstX(detected==1);
+    b = allObstY(detected==1);
+    detObst = horzcat(a, b);
+    unique(detObst, 'rows')
 
-a = allObstX(detected==1);
-b = allObstY(detected==1);
-detObst = horzcat(a, b)
+    occupancyMapNew = createOccupancyMap(a,b,xmax,ymax);
+    occupancyMap = occupancyMap | occupancyMapNew
+end
 
-
-occupancyMapNew = accumarray([b,a],1,[ymax xmax]);
-occupancyMap = occupancyMap | occupancyMapNew
-
- for r = 1:ymax
+i = 0;
+for r = 1:ymax
     for c = 1:xmax
-        if occupancyMap(r,c) == 1
-            obstList(r*c,:) = [r c];
-        elseif occupancyMap(r,c) == 0
-            obstList(r*c,:) = [0 0];
+        i = i+1;
+        cur = occupancyMap(r,c);
+        if cur == 1
+            obstList(i,:) = [c r];
+        elseif cur == 0
+            obstList(i,:) = [0 0];
         end
     end
- end
-obstList;
+end
+
+unique(obstList, 'rows');
