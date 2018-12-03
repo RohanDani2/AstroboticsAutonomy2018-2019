a72c6c4bf485146185173cd59af10155f65cef11
Colin Grundey <cgrundey@vt.edu>
2018-12-02 23:38:17 -0500

    Added working servo code. Updated README with links. ESC library added

--- teleop/README.md
+++ teleop/README.md
@@ -22,3 +22,20 @@ To be implemented
 ```cpp
 
 ```
+
+### Links
+  - [ESP8266 Guide](https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/using-arduino-ide)
+  - [ESP8266 Wifi UDP Guide](https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/udp-examples.html
+)
+  - [Python Connectivity Script](http://johnallen.us/?p=236)
+  - [JSON UDP](https://arduinojson.org/v5/example/udp-beacon/)
+  - [PWM Forum Post](https://arduino.stackexchange.com/questions/421/generating-pwm-signal-1-2-ms-333-hz
+)
+  - [VESC PPM Arduino](https://www.electric-skateboard.builders/t/rc-servo-ppm-controller-with-arduino/28581)
+  - [ESC Arduino](https://forum.arduino.cc/index.php?topic=270309.0)
+  -
+
+### Repositories to Use
+  - [Xbox Controller](https://github.com/FRC4564/Xbox)
+  - [ESP8266 RC Car](https://github.com/indrekots/esp8266-rc-car-controller)
+  - [VESC Arduino](https://github.com/shusain/eskatecontroller/)
--- teleop/Robot/Robot.ino
+++ teleop/Robot/Robot.ino
@@ -2,14 +2,10 @@
 #include <ESP8266WiFi.h>
 //#include <Wire.h>
 //#include <Adafruit_PWMServoDriver.h>
-//#include <Servo.h>
-
-
-//const char* ssid = "TigerIsBack"; //Enter your wifi network SSID
-//const char* password = "TigerIsBlack69"; //Enter your wifi network password
+#include <Servo.h>
 
 const char* ssid = "eduroam"; //Enter your wifi network SSID
-const char* password = "#74Landscape"; //Enter your wifi network password
+const char* password = ""; //Enter your wifi network password
 
 
 unsigned int localUDPport = 4210;
@@ -24,18 +20,24 @@ const int BUCKET_DIG = 2;
 const int BUCKET_LIFT = 3;
 const int DUMP_LIFT = 4;
 
+Servo right_drive, 
+      left_drive, 
+      bucket_dig, 
+      bucket_lift, 
+      dump_lift;
+
 byte packetBuffer[512];
 
 WiFiUDP Udp;
 
 
-void initOutputs() {
-  // MOTOR PWM
-  pinMode(LOCO_RIGHT, OUTPUT);
-  pinMode(LOCO_LEFT, OUTPUT);
-  pinMode(BUCKET_DIG, OUTPUT);
-  pinMode(BUCKET_LIFT, OUTPUT);
-  pinMode(DUMP_LIFT, OUTPUT);
+void init_motors() {
+  // PWM pins
+  right_drive.attach(0);
+  left_drive.attach(1);
+  bucket_dig.attach(2);
+  bucket_lift.attach(3);
+  dump_lift.attach(4);
 }
 
 void connectWifi() {
@@ -67,6 +69,7 @@ void loop() {
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    bucket_dig.write(45);
     // receive incoming UDP packets
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(packetBuffer, 255);
@@ -76,47 +79,9 @@ void loop() {
     }
     Serial.printf("UDP packet contents: %s\n", packetBuffer);
 
-    // send back a reply, to the IP address and port we got the packet from
+//    send back a reply, to the IP address and port we got the packet from
 //    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
 //    Udp.write(replyPacket);
 //    Udp.endPacket();
   }
 }
-
-/*
-#include <Servo.h>  //This comes with the Arduino
-
-int talon_pin = 3;
-
-Servo talon;
-*/
-
-/* The 'Servo.h' library allows you to control motors and servos by passing any value between
-0 and 180 into the "write" method (talon.write() in this program).
-For a motor:   0 is full power in one direction,
-             180 is full direction in the opposite direction,
-              90 is stop.
-For a servo:   0 is all the way one direction,
-             180 is all the way in the opposite direction,
-              90 is the middle.
-*/
-/*
-int talon_max_forward = 180;  //this might not actually be forward
-int talon_max_reverse = 00;    //this might not actually be reverse
-int talon_stop = 90;          //this is definitely stop (if the Talon is calibrated right)
-
-void setup() {
-  talon.attach(talon_pin);  //This tells the arduino to control this pin like it's a servo/motor
-}
-
-void loop() {
-  talon.write(talon_max_forward);   //full power one way
-  delay(2000);                      //wait 2 seconds
-  talon.write(talon_stop);          //full power the other way
-  delay(2000);                      //wait 2 seconds
-  talon.write(talon_max_reverse);   //full power one way
-  delay(2000);                      //wait 2 seconds
-  talon.write(talon_stop);          //full power the other way
-  delay(2000);                      //wait 2 seconds
-}
-*/
