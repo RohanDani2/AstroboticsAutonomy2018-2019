179e83ab2be4d0a8c470a6e242cdfab71a3a256e
alpetrovsky <acp2124@vt.edu>
2019-03-25 21:01:22 -0400

    working on serial communication with motor controlling arduino

---
+++ arduino/motor_con/motor_con.ino
@@ -0,0 +1,102 @@
+/* VT Astrobotics 2019
+   AUTONOMOUS RC CAR
+     Testing Autonomy on RC car through Serial communication
+     Drive motor: BLDC RC car ESC
+     Steering: Servo for left and right
+*/
+
+
+#include <Servo.h>
+
+/* Motor Pins */
+#define DRIVING_RIGHT    3
+#define DRIVING_LEFT     5
+
+#define BAUD_RATE   115200
+
+enum dir {
+  CW,
+  CC,
+  FOR,
+  BACK,
+  STOP
+};
+
+/* Constant speeds for testing purposes */
+const int stopSpeed = 1500;
+const int forwardSpeed = 1400;
+const int backwardSpeed = 1600;
+const int turnSpeed = 100;
+
+Servo driveMotor_Left;
+Servo driveMotor_Right;
+
+byte serialCMD;
+
+void init_motors() {
+  driveMotor_Left.attach(DRIVING_LEFT);
+  driveMotor_Right.attach(DRIVING_RIGHT);
+}
+
+/* Drive ESC with PPM */
+void drive(dir command) {
+  if (command == STOP) {
+    driveMotor_Left.writeMicroseconds(stopSpeed);
+    driveMotor_Right.writeMicroseconds(stopSpeed);
+  }else if (command == FOR) {
+    driveMotor_Left.writeMicroseconds(forwardSpeed);
+    driveMotor_Right.writeMicroseconds(forwardSpeed);
+  }else if (command == BACK) {
+    driveMotor_Left.writeMicroseconds(backwardSpeed);
+    driveMotor_Right.writeMicroseconds(backwardSpeed);
+  }else if (command == CC) {
+    driveMotor_Left.writeMicroseconds(stopSpeed-turnSpeed);
+    driveMotor_Right.writeMicroseconds(stopSpeed+turnSpeed);
+  }
+  else if (command == CW) {
+    driveMotor_Left.writeMicroseconds(stopSpeed+turnSpeed);
+    driveMotor_Right.writeMicroseconds(stopSpeed-turnSpeed);
+  }
+}
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  Serial.println("Begin");
+  delay(10);
+  init_motors();
+}
+
+void loop() {
+//  if (Serial.available() > 0) {
+//    serialCMD = Serial.read(); 
+//    Serial.println(Serial.available());
+//  }
+    int a = Serial.available();
+    Serial.println(a);
+//  Serial.println(0);
+//  Serial.println(1);
+
+//  if (serialCMD == 0) drive(STOP);
+//  else if (serialCMD == 1) drive(FOR);
+//  else if (serialCMD == 2) drive(BACK);
+//  else if (serialCMD == 3) drive(CC);
+//  else if (serialCMD == 4) drive(CW);
+
+//  drive(FOR);
+//  delay(2000);
+//  drive(STOP);
+//  delay(2000);
+//  drive(BACK);
+//  delay(2000);
+//  drive(STOP);
+//  delay(4000);
+//  drive(CC);
+//  delay(2000);
+//  drive(STOP);
+//  delay(2000);
+//  drive(CW);
+//  delay(2000);
+//  drive(STOP);
+//  delay(4000);
+
+}
--- simulink/anav_real/initialize.m
+++ simulink/anav_real/initialize.m
@@ -6,9 +6,26 @@ clf
 %% Config
 tic
 mode = 1; % 1 for simulation, 2 for motors and lidar, 3 for motors, lidar, and pozyx
+port_arduino_motors = '/dev/ttyACM0'; % serial port ID for motor control'/dev/ttyACM0'ling arduino 
 
 mapDim = [58 37]; % X and Y measurements of arena in whole decimeters
 scale = 100; % multiplier between system and lidar precision (mm to decimeter) 
+
+%% Set serial communication objects 
+W = evalin('base','whos');
+doesExist = ismember('COM_arduino_motors',{W(:).name});
+if doesExist
+    %fclose(COM_arduino_motors);
+    delete(COM_arduino_motors) 
+    clear COM_arduino_motors 
+end
+
+COM_arduino_motors = serial(port_arduino_motors);
+set(COM_arduino_motors, 'Timeout', 10)
+set(COM_arduino_motors, 'BaudRate', 115200)
+COM_arduino_motors.Terminator = {'CR/LF', 0};
+
+%fopen(COM_arduino_motors);
 
 %% Set colormap to inverted blue scale and generate surface point vectors 
 load('notbone.mat')
